a6791718cc10a6cbb5f30ae1e1df317cb1753590
Madhusudhan <marumakamadhu777@gmail.com>
2025-01-27 16:04:35 +0530

    second commit

--- prime.py
+++ prime.py
@@ -1,9 +1,27 @@
-n = int(input())
-c=0
-for i in range(1,n+1):
-    if n%i==0:
-        c+=1
-if c==2:
-    print("prime")
-else:
-    print("not a prime")
+# n = int(input())
+# c=0
+# for i in range(1,n+1):
+#     if n%i==0:
+#         c+=1
+# if c==2:
+#     print("prime")
+# else:
+#     print("not a prime")
+
+
+# prime number using while loop
+
+def prime(n):
+    i=1
+    c=0
+    while i<=n:
+        if n%i==0:
+            c+=1
+        i+=1
+    if c==2:
+        print("prime")
+    else:
+        print("not a prime")
+
+n =int(input())
+prime(n)
