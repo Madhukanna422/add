f765af21267b44f0b45ced6a94f851bcbcf2a4d5
Madhusudhan <marumakamadhu777@gmail.com>
2025-01-29 10:53:15 +0530

    python_mysql connection

---
+++ python_mysql2.py
@@ -0,0 +1,9 @@
+from mysql.connector import connection
+conn=connection.MySQLConnection(host="localhost",database="world",user="root",password="Shreyan@422")
+cursor=conn.cursor()
+query="select * from city where id=5"
+cursor.execute(query)
+for x in cursor:
+    print(x)
+cursor.close()
+conn.close()
